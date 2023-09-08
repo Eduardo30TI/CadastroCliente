e53b9e3c96710f6c76a86f9bec260d6acdfb8aa9
TI <121513063+Eduardo30TI@users.noreply.github.com>
2023-09-08 16:43:12 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -94,6 +94,12 @@ def main():
 
                     else:
 
+                        for k,v in temp_dict.items():
+
+                            temp_dict[k]=Acentuacao.RemoverAcento(v)
+
+                            pass
+
                         path_base=os.path.join(os.getcwd(),temp_dict['cnpj'])
                         os.makedirs(path_base,exist_ok=True)
                         temp_path=os.path.join(path_base,'Cadastro.xlsx')
